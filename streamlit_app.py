5976385c313e035681993a0a6b128235cbc7477f
merabtenehouria <44142046+merabtenehouria@users.noreply.github.com>
2024-02-17 14:11:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 kale, Spinach & Rocket Smoothie') 
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
